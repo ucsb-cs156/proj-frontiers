b413bb686b81ead1e836dc9cf7cf4cae5725e429
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-03-30 14:20:43 -0700

    pc - remove commented out code and unused imports

--- src/test/java/edu/ucsb/cs156/frontiers/controllers/JobsControllerSpyTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/controllers/JobsControllerSpyTests.java
@@ -1,17 +1,20 @@
 package edu.ucsb.cs156.frontiers.controllers;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -23,20 +26,7 @@ import edu.ucsb.cs156.frontiers.repositories.JobsRepository;
 import edu.ucsb.cs156.frontiers.repositories.UserRepository;
 import edu.ucsb.cs156.frontiers.services.UpdateUserService;
 import edu.ucsb.cs156.frontiers.services.jobs.JobService;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MvcResult;
 
 @Slf4j
 @WebMvcTest(controllers = JobsController.class)
@@ -76,13 +66,11 @@ public class JobsControllerSpyTests extends ControllerTestCase {
         .status("started")
         .build();
 
-    // when(jobsRepository.save(any(Job.class))).thenReturn(jobStarted).thenReturn(jobRunning).thenReturn(jobCompleted);
     when(jobService.runAsJob (any(UpdateAllJob.class))).thenReturn(jobStarted);
 
-    // doNothing().when(updateUserService).attachRosterStudentsAllUsers();
 
     // act
-    MvcResult response = mockMvc
+    mockMvc
         .perform(post("/api/jobs/launch/updateAll").with(csrf()))
         .andExpect(status().isOk())
         .andReturn();
