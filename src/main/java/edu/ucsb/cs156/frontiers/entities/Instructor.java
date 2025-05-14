ad2e22f70f19ca69d42ccf9f167a2a7703b07d1c
Hannya Yan <yilei_yan@ucsb.edu>
2025-05-14 16:41:13 -0700

    HY - create database table for Instructor

---
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Instructor.java
@@ -0,0 +1,17 @@
+package edu.ucsb.cs156.frontiers.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * This is a JPA entity that represents an Instructor.
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity(name = "instructors")
+public class Instructor {
+    @Id
+    private String email;
+}
