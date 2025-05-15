3005915e8f2a82267338ec0c85d504680f9346a9
Yuxiang Zhang <yuxiangzhang@ucsb.edu>
2025-05-14 19:32:35 -0700

    Create Instructor Table (entity and repository)

---
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Instructor.java
@@ -0,0 +1,18 @@
+package edu.ucsb.cs156.frontiers.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Instructor {
+
+    @Id
+    @Column(name = "EMAIL", nullable = false, unique = true, length = 255)
+    private String email;
+}
