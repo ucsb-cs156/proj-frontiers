3e34df4d019f5047d125e885315aa9a2e8c38dd0
Jay Joo <jayjoo@ucsb.edu>
2025-05-17 14:38:45 -0700

    jj - created RoleEmailForm for Admin and Instructors

---
+++ frontend/src/fixtures/roleEmailFormFixtures.js
@@ -0,0 +1,17 @@
+const roleEmailFormFixtures = {
+  oneRoleEmailForm: {
+    email: "joegaucho@ucsb.edu"
+  },
+  threeRoleEmailForms: [{
+    email: "joegaucho@ucsb.edu"
+  },
+  {
+    email: "rickgaucho@ucsb.edu"
+  },
+  {
+    email: "georgegaucho@ucsb.edu"
+  }
+  ],
+};
+
+export { roleEmailFormFixtures };
---
+++ frontend/src/stories/components/Users/RoleEmailForm.stories.js
@@ -0,0 +1,34 @@
+import React from "react";
+import RoleEmailForm from "main/components/Users/RoleEmailForm";
+import { roleEmailFormFixtures } from "fixtures/roleEmailFormFixtures";
+
+export default {
+  title: "components/Users/roleEmailFormFixtures",
+  component: roleEmailFormFixtures,
+};
+
+const Template = (args) => {
+  return <RoleEmailForm {...args} />;
+};
+
+export const Create = Template.bind({});
+
+Create.args = {
+  buttonLabel: "Create",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
+
+export const Update = Template.bind({});
+
+Update.args = {
+  initialContents:
+    roleEmailFormFixtures.oneRoleEmailForm,
+  buttonLabel: "Update",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
