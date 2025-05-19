cbb38ba694d288e56605d3ee79f67ad4ff8d4600
Andrew <andrewkim102@ucsb.edu>
2025-05-19 02:39:37 -0700

    AK - wip, since package lines are bugging(will ask TA), but the skeleton code and general gist is present

---
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Admin.java
@@ -0,0 +1,19 @@
+package edu.ucsb.cs156.frontiers.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
