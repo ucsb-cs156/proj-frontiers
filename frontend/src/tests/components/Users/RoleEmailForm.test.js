8596909dbbf44fae9cb14a2d8f3d5305d1f35e74
Ruben Alvarez-Gutierrez <ruben513@ucsb.edu>
2025-05-19 15:37:23 -0700

    ra-RoleEmailForm, tests and stories

---
+++ frontend/src/tests/components/Users/RoleEmailForm.test.js
@@ -0,0 +1,81 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import RoleEmailForm from "main/components/Users/RoleEmailForm";
+import { _roleEmailFixtures } from "fixtures/roleEmailFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("RoleEmailForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = ["Email"];
+  const testId = "RoleEmailForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <RoleEmailForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Add Email/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <RoleEmailForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <RoleEmailForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Add Email/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Add Email/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Email is required./);
+    expect(screen.getByText(/Email is required/)).toBeInTheDocument();
+
+    const testInput = screen.getByTestId(`${testId}-email`);
+    fireEvent.change(testInput, { target: { value: "notanemail" } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/Please enter a valid email address./),
+      ).toBeInTheDocument();
+    });
+  });
+});
