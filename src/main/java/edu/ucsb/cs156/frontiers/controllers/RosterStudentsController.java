5ffd8375350be5c68946ac819f54706977968a24
Arthur Wu <30936426+arthurwu17@users.noreply.github.com>
2025-05-18 17:23:05 -0700

    Update RosterStudentsController.java
    added import for delete mapping

--- src/main/java/edu/ucsb/cs156/frontiers/controllers/RosterStudentsController.java
+++ src/main/java/edu/ucsb/cs156/frontiers/controllers/RosterStudentsController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
