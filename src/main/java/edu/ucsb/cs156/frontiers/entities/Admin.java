98892946fadd3b670143f2789dc657759908e109
Hung Khuu <hungkhuu@ucsb.edu>
2025-05-20 18:44:54 -0700

    hk - backend CRUD operations for Admin Table

--- src/main/java/edu/ucsb/cs156/frontiers/entities/Admin.java
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Admin.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.frontiers.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -9,6 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "ADMINS")
 public class Admin {
     @Id
     private String email;
