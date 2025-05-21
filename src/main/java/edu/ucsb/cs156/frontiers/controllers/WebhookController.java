cfade52db543a74529f78fad8a937ef393e83f63
Shelly Zhu <zhixiuzhu@ucsb.edu>
2025-05-20 21:46:47 -0700

    logs

--- src/main/java/edu/ucsb/cs156/frontiers/controllers/WebhookController.java
+++ src/main/java/edu/ucsb/cs156/frontiers/controllers/WebhookController.java
@@ -11,6 +11,7 @@ import edu.ucsb.cs156.frontiers.repositories.CourseRepository;
 import edu.ucsb.cs156.frontiers.repositories.RosterStudentRepository;
 import edu.ucsb.cs156.frontiers.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import java.util.Optional;
 @Tag(name = "Webhooks Controller")
 @RestController
 @RequestMapping("/api/webhooks")
+@Slf4j
 public class WebhookController {
 
 
@@ -41,9 +43,11 @@ public class WebhookController {
     */
     @PostMapping("/github")
     public ResponseEntity<String> createGitHubWebhook(@RequestBody JsonNode jsonBody) throws JsonProcessingException {
+        log.info("Received GitHub webhook: {}", jsonBody.toString());
 
         if(jsonBody.has("action")){
             String action = jsonBody.get("action").asText();
+            log.info("Webhook action: {}", action);
             
             // Handle member_added and member_invited events
             if(action.equals("member_added") || action.equals("member_invited")){
@@ -54,24 +58,39 @@ public class WebhookController {
                     
                     String githubLogin = jsonBody.get("membership").get("user").get("login").asText();
                     String installationId = jsonBody.get("installation").get("id").asText();
+                    log.info("GitHub login: {}, Installation ID: {}", githubLogin, installationId);
+                    
                     Optional<Course> course = courseRepository.findByInstallationId(installationId);
+                    log.info("Course found: {}", course.isPresent());
                     
                     if(course.isPresent()){
                         Optional<RosterStudent> student = rosterStudentRepository.findByCourseAndGithubLogin(course.get(), githubLogin);
+                        log.info("Student found: {}", student.isPresent());
+                        
                         if(student.isPresent()){
                             RosterStudent updatedStudent = student.get();
+                            log.info("Current student org status: {}", updatedStudent.getOrgStatus());
                             
                             // Update status based on action
                             if(action.equals("member_added")) {
                                 updatedStudent.setOrgStatus(OrgStatus.MEMBER);
+                                log.info("Setting status to MEMBER");
                             } else if(action.equals("member_invited")) {
                                 updatedStudent.setOrgStatus(OrgStatus.INVITED);
+                                log.info("Setting status to INVITED");
                             }
                             
                             rosterStudentRepository.save(updatedStudent);
+                            log.info("Student saved with new org status: {}", updatedStudent.getOrgStatus());
                             return ResponseEntity.ok(updatedStudent.toString());
+                        } else {
+                            log.warn("No student found with GitHub login: {} in course: {}", githubLogin, course.get().getCourseName());
                         }
+                    } else {
+                        log.warn("No course found with installation ID: {}", installationId);
                     }
+                } else {
+                    log.warn("Webhook missing required fields for processing");
                 }
             }
         }
