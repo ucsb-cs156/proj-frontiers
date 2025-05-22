8fc498da0a2181030d7e8ac185226bf11a9dbf53
Jason Zhao <jasonzhao@ucsb.edu>
2025-05-22 13:54:40 -0700

    jz - added instructor controller class with working get, post, and delete

---
+++ src/main/java/edu/ucsb/cs156/frontiers/controllers/InstructorController.java
@@ -0,0 +1,71 @@
+package edu.ucsb.cs156.frontiers.controllers;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import edu.ucsb.cs156.frontiers.entities.Instructor;
+import edu.ucsb.cs156.frontiers.errors.EntityNotFoundException;
+import edu.ucsb.cs156.frontiers.repositories.InstructorRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name = "Instructors")
+@RequestMapping("/api/instructor")
+@RestController
+@Slf4j
+public class InstructorController extends ApiController{
+    @Autowired
+    InstructorRepository instructorRepository;
+
+    @Operation(summary= "List all instructors")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/all")
+    public Iterable<Instructor> allInstructors() {
+        Iterable<Instructor> instructor = instructorRepository.findAll();
+        return instructor;
+    }
+
+    @Operation(summary= "Create a new instructor")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public Instructor postInstructor(
+    @Parameter(name="email") @RequestParam String email)
+    throws JsonProcessingException {
+
+
+
+    Instructor instructor = new Instructor();
+    instructor.setEmail(email);
+
+    Instructor savedInstructor = instructorRepository.save(instructor);
+
+    return savedInstructor;
+    }
+
+    @Operation(summary= "Delete an Instructor")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteInstructor(@Parameter(name="email") @RequestParam String email) {
+        
+        Instructor  instructor = instructorRepository.findById(email)
+                .orElseThrow(() -> new EntityNotFoundException(Instructor.class, email));
+
+        instructorRepository.delete(instructor);
+        return genericMessage("Instructor with valid email is deleted".formatted(email));
+    }
+
+
+}
