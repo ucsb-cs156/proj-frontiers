3598c339f736e6321a65c7f718d5289bb9d4e028
Phill Conrad <pconrad@cs.ucsb.edu>
2025-07-12 15:52:12 -0700

    pc - bring over code from proj-courses to handle storybook stories with params in the route

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -5,40 +5,53 @@ import 'react-toastify/dist/ReactToastify.css';
 import { initialize, mswLoader } from 'msw-storybook-addon'
 
 import { QueryClient, QueryClientProvider } from "react-query";
-import { MemoryRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { MemoryRouter, useLocation } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
-const currentUrl = window.location.href;
-const isLocalhost = currentUrl.startsWith("http://localhost:6006/");
-const mockServiceWorkerUrl = isLocalhost ? "mockServiceWorker.js" : "https://" + window.location.hostname + "/mockServiceWorker.js";
-
 // Initialize MSW
+initialize()
 
-initialize(
-  {
-    serviceWorker: {
-      url: mockServiceWorkerUrl
-    }
-  }
-);
-
-
-// Per https://storybook.js.org/docs/react/writing-stories/decorators#context-for-mocking
-// Here, we provide the context needed for some of the components,
-// e.g. the ones that rely on currentUser
+// For conditional decorators trick, see: https://github.com/storybookjs/storybook/issues/23237#issuecomment-1611351405 
+// Decorators are applied in order; the innermost decorator is applied first.
+// Here, if suppressMemoryRouter is true, then the MemoryRouter decorator is not applied,
+// and we don't use the useLocation hook to show a toast message when navigate is called.
 
 export const decorators = [
+  (Story, Context) => {
+    if (Context.args?.suppressMemoryRouter) {
+      return <Story />;
+    } else {
+      const location = useLocation();
+      useEffect(() => {
+        if (location.pathname !== "/") {
+          toast("Would navigate to: " + location.pathname);
+        }
+      }, [location]);
+      return <Story />;
+    }
+  },
+  (Story) => {
+    return (<>
+      <ToastContainer />
+      <Story />
+    </>
+    );
+  },
+  (Story, Context) => (
+    Context.args?.suppressMemoryRouter ?
+      <Story /> :
+      <MemoryRouter><Story /></MemoryRouter>
+  ),
   (Story) => (
     <QueryClientProvider client={queryClient}>
-      <MemoryRouter>
-        <ToastContainer />
-        <Story />
-      </MemoryRouter>
-    </QueryClientProvider>
-  )
+      <Story />
+    </QueryClientProvider >
+  ),
 ];
+
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -52,5 +65,6 @@ const preview = {
   },
   loaders: [mswLoader]
 };
+
 
 export default preview;
