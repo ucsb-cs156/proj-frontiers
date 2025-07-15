21a75d54773f129a2bbe572cdbdf89ccfd5bff8e
Daniel Jensen <djensen2@outlook.com>
2025-07-15 16:31:54 -0700

    dj - updated javadoc comment

--- src/test/java/edu/ucsb/cs156/frontiers/annotations/WithInstructorCoursePermissions.java
+++ src/test/java/edu/ucsb/cs156/frontiers/annotations/WithInstructorCoursePermissions.java
@@ -9,7 +9,7 @@ import java.lang.annotation.RetentionPolicy;
  * This annotation interacts with the mock CourseSecurity annotation. It adds authorities that tell the CourseSecurity annotation
  * used in testing that the user has permission to manage the course being accessed. It should be used on methods that have the
  * <i>@PreAuthorize("@CourseSecurity.hasManagePermissions")</i> annotation.
- * It cannot be combined with <i>@WithMockUser</i>.
+ * This should not be combined with <i>@WithMockUser</i>, because this annotation implies <i>@WithMockUser("ROLE_INSTRUCTOR")</i>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @WithMockUser(authorities = {"COURSE_PERMISSION","ROLE_INSTRUCTOR"})
