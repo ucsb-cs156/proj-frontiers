9b0e884b75f2dda37cd3ab5878d281cf6957eb1c
Daniel Jensen <djensen2@outlook.com>
2025-07-16 14:43:38 -0700

    dj - added method to unenroll frontiers from a GitHub org

--- src/test/java/edu/ucsb/cs156/frontiers/services/OrganizationLinkerServiceTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/services/OrganizationLinkerServiceTests.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.frontiers.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import edu.ucsb.cs156.frontiers.entities.Course;
 import edu.ucsb.cs156.frontiers.errors.InvalidInstallationTypeException;
 import edu.ucsb.cs156.frontiers.services.wiremock.WiremockService;
 import edu.ucsb.cs156.frontiers.testconfig.TestConfig;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -19,9 +21,8 @@ import java.security.spec.InvalidKeySpecException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
 @RestClientTest(OrganizationLinkerService.class)
 @Import({TestConfig.class})
@@ -113,6 +114,50 @@ public class OrganizationLinkerServiceTests {
         });
 
         assertEquals(expectedMessage, thrownException.getMessage());
+    }
+
+    @Test
+    public void earlyReturnOnNoInstallationId() throws Exception{
+        Course course = Course.builder().orgName("ucsb-cs156").build();
+        organizationLinkerService.unenrollOrganization(course);
+        mockRestServiceServer.verify();
+    }
+
+    @Test
+    public void earlyReturnOnNoOrgName() throws Exception{
+        Course course = Course.builder().build();
+        organizationLinkerService.unenrollOrganization(course);
+        mockRestServiceServer.verify();
+    }
+
+    @Test
+    public void testUnenrollOrganization() throws Exception {
+        Course course = Course.builder().orgName("ucsb-cs156").installationId("123456").build();
+
+        doReturn("definitely.real.jwt").when(jwtService).getJwt();
+        mockRestServiceServer.expect(requestTo("https://api.github.com/app/installations/123456"))
+                .andExpect(method(HttpMethod.DELETE))
+                .andExpect(header("Authorization", "Bearer definitely.real.jwt" ))
+                .andExpect(header("Accept", "application/vnd.github+json"))
+                .andExpect(header("X-GitHub-Api-Version", "2022-11-28"))
+                .andRespond(withNoContent());
+        organizationLinkerService.unenrollOrganization(course);
+        mockRestServiceServer.verify();
+    }
+
+    @Test
+    public void testNotInstalled() throws Exception {
+        Course course = Course.builder().orgName("ucsb-cs156").installationId("123456").build();
+
+        doReturn("definitely.real.jwt").when(jwtService).getJwt();
+        mockRestServiceServer.expect(requestTo("https://api.github.com/app/installations/123456"))
+                .andExpect(method(HttpMethod.DELETE))
+                .andExpect(header("Authorization", "Bearer definitely.real.jwt" ))
+                .andExpect(header("Accept", "application/vnd.github+json"))
+                .andExpect(header("X-GitHub-Api-Version", "2022-11-28"))
+                .andRespond(withResourceNotFound());
+        organizationLinkerService.unenrollOrganization(course);
+        mockRestServiceServer.verify();
     }
 
 }
