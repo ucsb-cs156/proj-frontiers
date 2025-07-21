7812a6e561ff6fe6024fe30f802614ce41a35053
Divyani Punj <divyanipunj@ucsb.edu>
2025-07-21 14:07:26 -0700

    dp - tooltips in progress

--- frontend/src/main/components/Courses/CoursesTable.js
+++ frontend/src/main/components/Courses/CoursesTable.js
@@ -1,5 +1,5 @@
 import OurTable from "main/components/OurTable";
-import { Button } from "react-bootstrap";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const columns = [
   {
@@ -21,8 +21,18 @@ const columns = [
 ];
 
 export default function CoursesTable({ courses, testId, storybook = false }) {
+   const cellToAxiosParamsJoinCourse = (cell) => {
+    return {
+      url: 'api/rosterstudents/joinCourse',
+      method: "PUT",
+      params: {
+        rosterStudentId: cell.row.original.rosterStudentId,
+      },
+    };
+  };
   const joinCallback = (cell) => {
     // TODO: Implement the join functionality here
+    
     if (storybook) {
       window.alert(
         `Join callback invoked for course with id: ${cell.row.values.id}`,
@@ -37,6 +47,34 @@ export default function CoursesTable({ courses, testId, storybook = false }) {
     window.open(gitInvite, "_blank");
   };
 
+  const renderTooltip = (studentStatus) => (props) => {
+    let set_message;
+
+    switch (studentStatus) {
+      case "PENDING":
+        set_message = "This course has not been completely set up by your instructor yet."; 
+        break; 
+      case "JOINCOURSE":
+        set_message = "Clicking this button will generate an invitation to the GitHub organization associated with this course."; 
+        break; 
+      case "INVITED":
+        set_message = "You have been invited to the GitHub organization associated with this course, but you still need to accept or decline the invitation. Please accept it if you plan to stay enrolled, and decline only if you plan to withdraw from the course."; 
+        break; 
+      case "OWNER":
+        set_message = "You are an owner of the GitHub organization associated with this course."; 
+        break; 
+      case "MEMBER":
+        set_message = "You are a member of the GitHub organization associated with this course."; 
+        break; 
+    }
+    return (
+      <Tooltip id={`${studentStatus.toLowerCase()}-tooltip`} {...props}>
+        {set_message}
+      </Tooltip>
+    );
+  };
+
+
   const columnsWithStatus = [
     ...columns,
     {
@@ -44,31 +82,66 @@ export default function CoursesTable({ courses, testId, storybook = false }) {
       accessor: "studentStatus",
       Cell: ({ cell }) => {
         if (cell.value === "PENDING") {
-          return <span style={{ color: "orange" }}>Pending</span>;
+          return( 
+          <OverlayTrigger
+            placement="right"
+            overlay={renderTooltip("PENDING")}
+          >
+             <span style={{ color: "orange" }}>Pending</span>
+          </OverlayTrigger>
+          )
         } else if (cell.value === "JOINCOURSE") {
           return (
-            <Button
+            <OverlayTrigger
+              placement="right"
+              overlay={renderTooltip("JOINCOURSE")}
+            >
+            <span>
+             <Button
               variant={"primary"}
               onClick={() => joinCallback(cell)}
               data-testid={`${testId}-cell-row-${cell.row.index}-col-${cell.column.id}-button`}
             >
               Join Course
             </Button>
+            </span>
+            </OverlayTrigger>
           );
         } else if (cell.value === "INVITED") {
           return (
-            <Button
+            <OverlayTrigger
+              placement="right"
+              overlay={renderTooltip("INVITED")}
+            >
+             <span>
+             <Button
               variant={"primary"}
               onClick={() => viewInviteCallback(cell)}
               data-testid={`${testId}-cell-row-${cell.row.index}-col-${cell.column.id}-button`}
             >
               View Invite
             </Button>
+            </span>
+            </OverlayTrigger>
           );
         } else if (cell.value === "OWNER") {
-          return <span style={{ color: "purple" }}>Owner</span>;
+          return (
+            <OverlayTrigger
+              placement="right"
+              overlay={renderTooltip("OWNER")}
+            >
+            <span style={{ color: "purple" }}>Owner</span>
+          </OverlayTrigger>
+          )
         } else if (cell.value === "MEMBER") {
-          return <span style={{ color: "blue" }}>Member</span>;
+          return(
+            <OverlayTrigger
+              placement="right"
+              overlay={renderTooltip("MEMBER")}
+            >
+            <span style={{ color: "blue" }}>Member</span>
+          </OverlayTrigger>
+          )
         }
         return <span>{cell.value}</span>;
       },
--- frontend/src/tests/components/Courses/CoursesTable.test.js
+++ frontend/src/tests/components/Courses/CoursesTable.test.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent, act} from "@testing-library/react";
 import coursesFixtures from "fixtures/coursesFixtures";
 import CoursesTable from "main/components/Courses/CoursesTable";
 import { BrowserRouter } from "react-router-dom";
@@ -93,8 +93,9 @@ describe("CoursesTable tests", () => {
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent("Join Course");
     expect(button).toHaveAttribute("class", "btn btn-primary");
-
-    fireEvent.click(button);
+    act(() => {
+      fireEvent.click(button);
+    })
     expect(window.alert).not.toHaveBeenCalled();
   });
 
@@ -115,7 +116,10 @@ describe("CoursesTable tests", () => {
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent("Join Course");
     expect(button).toHaveAttribute("class", "btn btn-primary");
-    fireEvent.click(button);
+    
+    act(() => {
+      fireEvent.click(button);
+    })
     await waitFor(() => {
       expect(window.alert).toHaveBeenCalledTimes(1);
     });
@@ -142,7 +146,9 @@ describe("CoursesTable tests", () => {
     expect(button).toHaveTextContent("Join Course");
     expect(button).toHaveAttribute("class", "btn btn-primary");
 
-    fireEvent.click(button);
+    act(() => {
+      fireEvent.click(button);
+    })
     expect(window.alert).not.toHaveBeenCalled();
   });
 
@@ -164,7 +170,9 @@ describe("CoursesTable tests", () => {
     expect(button).toHaveTextContent("View Invite");
     expect(button).toHaveAttribute("class", "btn btn-primary");
 
-    fireEvent.click(button);
+    act(() => {
+      fireEvent.click(button);
+    })
     expect(window.alert).not.toHaveBeenCalled();
   });
 
@@ -206,12 +214,184 @@ describe("CoursesTable tests", () => {
     expect(button).toHaveTextContent("View Invite");
     expect(button).toHaveAttribute("class", "btn btn-primary");
 
+    act(() => {
     fireEvent.click(button);
+    })
     expect(window.alert).not.toHaveBeenCalled();
 
     expect(openMock).toHaveBeenCalledWith(
       "https://github.com/ucsb-cs156-f25/invitation",
       "_blank",
     );
+  });
+  // test("tooltips for INVITED status", () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <CoursesTable
+  //         courses={coursesFixtures.oneCourseWithEachStatus}
+  //         storybook={false}
+  //         testId={"CoursesTable"}
+  //       />
+  //     </BrowserRouter>,
+  //   );
+    
+  //   jest.useFakeTimers(); 
+    
+  //   act(() => {
+  //     fireEvent.mouseOver(screen.getByText('Pending')); 
+  //   })
+  //   expect(screen.queryByText("This course has not been completely set up by your instructor yet.")).toBeNull(); 
+
+  //   // fireEvent.mouseOver(screen.getByText('Join Course')); 
+  //   // expect(screen.queryByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeNull(); 
+
+  //   act(() => {
+  //     jest.advanceTimersByTime(250);
+  //   }) 
+
+  //   expect(screen.getByText("This course has not been completely set up by your instructor yet.")).toBeInTheDocument();  
+  //   // expect(screen.queryByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeInTheDocument(); 
+
+  //   jest.useRealTimers(); 
+  // });
+  //   test("tooltips for JOINCOURSE status", () => {
+  //   render(
+  //     <BrowserRouter>
+  //       <CoursesTable
+  //         courses={coursesFixtures.oneCourseWithEachStatus}
+  //         storybook={false}
+  //         testId={"CoursesTable"}
+  //       />
+  //     </BrowserRouter>,
+  //   );
+    
+  //   jest.useFakeTimers(); 
+    
+  //   act(() => {
+  //     fireEvent.mouseOver(screen.getByText('Join Course')); 
+  //   })
+  //   expect(screen.queryByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeNull(); 
+
+  //   // fireEvent.mouseOver(screen.getByText('Join Course')); 
+  //   // expect(screen.queryByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeNull(); 
+
+  //   act(() => {
+  //     jest.advanceTimersByTime(250);
+  //   }) 
+
+  //   expect(screen.getByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeInTheDocument();  
+  //   // expect(screen.queryByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeInTheDocument(); 
+
+  //   jest.useRealTimers(); 
+  // });
+   test("tooltips for PENDING status", () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('Pending')); 
+    })
+    expect(screen.getByText("This course has not been completely set up by your instructor yet.")).toBeInTheDocument(); 
+  });
+  test("tooltips for JOINCOURSE status", () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('Join Course')); 
+    })
+    expect(screen.getByText("Clicking this button will generate an invitation to the GitHub organization associated with this course.")).toBeInTheDocument(); 
+  });
+  test("tooltips for INVITED status", () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('View Invite')); 
+    })
+    expect(screen.getByText("You have been invited to the GitHub organization associated with this course, but you still need to accept or decline the invitation. Please accept it if you plan to stay enrolled, and decline only if you plan to withdraw from the course.")).toBeInTheDocument(); 
+  });
+  test("tooltips for OWNER status", () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('Owner')); 
+    })
+    expect(screen.getByText("You are an owner of the GitHub organization associated with this course.")).toBeInTheDocument(); 
+  });
+  test("tooltips for MEMBER status", () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('Member')); 
+    })
+    expect(screen.getByText("You are a member of the GitHub organization associated with this course.")).toBeInTheDocument(); 
+  });
+    test("expect the correct tooltip ID", async () => {
+    render(
+      <BrowserRouter>
+        <CoursesTable
+          courses={coursesFixtures.oneCourseWithEachStatus}
+          storybook={false}
+          testId={"CoursesTable"}
+        />
+      </BrowserRouter>,
+    );
+    
+    jest.useFakeTimers(); 
+    
+    act(() => {
+      fireEvent.mouseOver(screen.getByText('Member')); 
+    })
+
+    const tooltip = await screen.findByRole('tooltip')
+    expect(tooltip).toHaveAttribute('id', 'member-tooltip')
   });
 });
