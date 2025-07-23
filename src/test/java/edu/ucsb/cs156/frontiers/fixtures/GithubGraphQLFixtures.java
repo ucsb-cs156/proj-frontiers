19d814e33506f0cdd2e6b814611044a90a33ec99
Phill Conrad <pconrad@cs.ucsb.edu>
2025-07-22 19:14:00 -0700

    pc - add endpoint and service to get commits

---
+++ src/test/java/edu/ucsb/cs156/frontiers/fixtures/GithubGraphQLFixtures.java
@@ -0,0 +1,81 @@
+package edu.ucsb.cs156.frontiers.fixtures;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class GithubGraphQLFixtures {
+
+
+
+
+  public static final ObjectMapper objectMapper = new ObjectMapper();
+  public static final String COMMITS_RESPONSE = """
+      {
+        "repository" : {
+          "ref" : {
+            "target" : {
+              "history" : {
+                "pageInfo" : {
+                  "hasNextPage" : true,
+                  "endCursor" : "79a22f53b228b10212b44f1a59c84a2999c23ecf 1"
+                },
+                "edges" : [ {
+                  "node" : {
+                    "oid" : "79a22f53b228b10212b44f1a59c84a2999c23ecf",
+                    "url" : "https://github.com/ucsb-cs156/proj-frontiers/commit/79a22f53b228b10212b44f1a59c84a2999c23ecf",
+                    "messageHeadline" : "Update azure-active-directory.md - formatting and clarification fixes",
+                    "committedDate" : "2025-07-22T22:26:12Z",
+                    "author" : {
+                      "name" : "Phill Conrad",
+                      "email" : "pconrad@cs.ucsb.edu",
+                      "user" : {
+                        "login" : "pconrad"
+                      }
+                    },
+                    "committer" : {
+                      "name" : "GitHub",
+                      "email" : "noreply@github.com",
+                      "user" : null
+                    }
+                  }
+                }, {
+                  "node" : {
+                    "oid" : "13f8c8a8821b3f0636597f2d42d2359ad34183b5",
+                    "url" : "https://github.com/ucsb-cs156/proj-frontiers/commit/13f8c8a8821b3f0636597f2d42d2359ad34183b5",
+                    "messageHeadline" : "Merge pull request #228 from ucsb-cs156/dj-addAppropriateOrgStatus",
+                    "committedDate" : "2025-07-22T22:04:48Z",
+                    "author" : {
+                      "name" : "Phill Conrad",
+                      "email" : "pconrad@cs.ucsb.edu",
+                      "user" : {
+                        "login" : "pconrad"
+                      }
+                    },
+                    "committer" : {
+                      "name" : "GitHub",
+                      "email" : "noreply@github.com",
+                      "user" : null
+                    }
+                  }
+                } ]
+              }
+            }
+          }
+        }
+      }
+      """;
+
+  
+  public static final Map<String, Object> COMMITS_RESPONSE_MAP;
+  static {
+    try {
+      COMMITS_RESPONSE_MAP = objectMapper.readValue(COMMITS_RESPONSE,
+        new TypeReference<Map<String, Object>>() {});
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
--- src/test/java/edu/ucsb/cs156/frontiers/services/GithubGraphQLServiceTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/services/GithubGraphQLServiceTests.java
@@ -5,71 +5,158 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import java.util.Map;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.graphql.client.HttpSyncGraphQlClient;
 import org.springframework.graphql.client.HttpSyncGraphQlClient.Builder;
+
+import org.springframework.graphql.GraphQlResponse;
+import org.springframework.graphql.client.ClientGraphQlResponse;
 import org.springframework.graphql.client.GraphQlClient.RequestSpec;
 import org.springframework.graphql.client.GraphQlClient.RetrieveSyncSpec;
 
 import edu.ucsb.cs156.frontiers.entities.Course;
+import edu.ucsb.cs156.frontiers.fixtures.GithubGraphQLFixtures;
 
 @ExtendWith(MockitoExtension.class)
 public class GithubGraphQLServiceTests {
 
-    @Mock
-    private JwtService jwtService;
+        @Mock
+        private JwtService jwtService;
 
-    @Mock
-    private HttpSyncGraphQlClient graphQlClient;
+        @Mock
+        private HttpSyncGraphQlClient graphQlClient;
 
-    @Mock
-    private HttpSyncGraphQlClient.Builder graphQlClientBuilder;
+        @Mock
+        private HttpSyncGraphQlClient.Builder graphQlClientBuilder;
 
-    @Mock
-    private RequestSpec requestSpec;
+        @Mock
+        private RequestSpec requestSpec;
 
-    @Mock
-    private RetrieveSyncSpec retrieveSyncSpec;
+        @Mock
+        private RetrieveSyncSpec retrieveSyncSpec;
 
-    Course course = Course.builder()
-            .id(1L)
-            .installationId("12345")
-            .orgName("test-org")
-            .courseName("Test Course")
-            .term("Fall 2023")
-            .school("UCSB")
-            .build();
+        @Mock
+        private ClientGraphQlResponse clientGraphQlResponse;
 
-    @Test
-    public void testGetDefaultBranchName() throws Exception {
-        assertNotNull(course);
-        when(jwtService.getInstallationToken(eq(course))).thenReturn("mocked-token");
+        Course course = Course.builder()
+                        .id(1L)
+                        .installationId("12345")
+                        .orgName("test-org")
+                        .courseName("Test Course")
+                        .term("Fall 2023")
+                        .school("UCSB")
+                        .build();
 
-        when(graphQlClient.mutate()).thenReturn(graphQlClientBuilder);
-        when(graphQlClientBuilder.header(anyString(), anyString()))
-                .thenReturn(graphQlClientBuilder);
-        when(graphQlClientBuilder.build())
-                .thenReturn(graphQlClient);
-        when(graphQlClient.document(anyString()))
-                .thenReturn(requestSpec);
-        when(requestSpec.variable(anyString(), anyString()))
-                .thenReturn(requestSpec);
-        when(requestSpec.retrieveSync(anyString()))
-                .thenReturn(retrieveSyncSpec);
-        when(retrieveSyncSpec.toEntity(eq(String.class)))
-                .thenReturn("main");
+        @BeforeEach
+        public void setup() {
+                MockitoAnnotations.openMocks(this);
+        }
 
-        GithubGraphQLService githubGraphQLService = new GithubGraphQLService(
-                graphQlClient,
-                jwtService);
-        String defaultBranchName = githubGraphQLService.getDefaultBranchName(course, "test-owner", "test-repo");
-        assertEquals("main", defaultBranchName);
-    }
-   
+        @AfterEach
+        public void tearDown() {
+                Mockito.reset(jwtService, graphQlClient, graphQlClientBuilder, requestSpec, retrieveSyncSpec,
+                                clientGraphQlResponse); // Reset specific mocks
+        }
+
+        @Test
+        public void testGetDefaultBranchName() throws Exception {
+                assertNotNull(course);
+                when(jwtService.getInstallationToken(eq(course))).thenReturn("mocked-token");
+
+                when(graphQlClient.mutate()).thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.header(anyString(), anyString()))
+                                .thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.build())
+                                .thenReturn(graphQlClient);
+                when(graphQlClient.document(anyString()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.variable(anyString(), anyString()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.retrieveSync(anyString()))
+                                .thenReturn(retrieveSyncSpec);
+                when(retrieveSyncSpec.toEntity(eq(String.class)))
+                                .thenReturn("main");
+
+                GithubGraphQLService githubGraphQLService = new GithubGraphQLService(
+                                graphQlClient,
+                                jwtService);
+                String defaultBranchName = githubGraphQLService.getDefaultBranchName(course, "test-owner", "test-repo");
+                assertEquals("main", defaultBranchName);
+        }
+
+        @Test
+        public void testGetCommits() throws Exception {
+
+                TypeReference<Map<String, Object>> typeRef = new TypeReference<Map<String, Object>>() {
+                };
+
+                when(clientGraphQlResponse.getData()).thenReturn(GithubGraphQLFixtures.COMMITS_RESPONSE_MAP);
+
+                when(graphQlClient.mutate()).thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.header(anyString(), anyString()))
+                                .thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.build())
+                                .thenReturn(graphQlClient);
+                when(graphQlClient.document(anyString()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.variable(anyString(), any()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.executeSync())
+                                .thenReturn(clientGraphQlResponse);
+                when(requestSpec.retrieveSync(anyString()))
+                                .thenReturn(retrieveSyncSpec);
+
+                assertNotNull(course);
+                when(jwtService.getInstallationToken(eq(course))).thenReturn("mocked-token");
+
+                when(graphQlClient.mutate()).thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.header(anyString(), anyString()))
+                                .thenReturn(graphQlClientBuilder);
+                when(graphQlClientBuilder.build())
+                                .thenReturn(graphQlClient);
+                when(graphQlClient.document(anyString()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.variable(anyString(), anyString()))
+                                .thenReturn(requestSpec);
+                when(requestSpec.executeSync())
+                                .thenReturn(clientGraphQlResponse);
+                when(requestSpec.retrieveSync(anyString()))
+                                .thenReturn(retrieveSyncSpec);
+
+                when(clientGraphQlResponse.getData())
+                                .thenReturn(GithubGraphQLFixtures.COMMITS_RESPONSE_MAP);
+
+                GithubGraphQLService githubGraphQLService = new GithubGraphQLService(
+                                graphQlClient,
+                                jwtService);
+                String commitsJson = githubGraphQLService.getCommits(course, "test-owner", "test-repo", "main", 10,
+                                null);
+
+                verify(graphQlClient).mutate();
+                verify(graphQlClientBuilder).header("Authorization", "Bearer mocked-token");
+                verify(graphQlClientBuilder).header("Content-Type", "application/json");
+                verify(graphQlClientBuilder).build();
+                verify(requestSpec).variable("owner", "test-owner");
+                verify(requestSpec).variable("repo", "test-repo");
+                verify(requestSpec).variable("branch", "main");
+                verify(requestSpec).variable("first", 10);
+                verify(requestSpec).variable("after", null);
+                verify(requestSpec).executeSync();
+                verify(clientGraphQlResponse).getData();
+
+                assertEquals(GithubGraphQLFixtures.COMMITS_RESPONSE.trim(), commitsJson.trim());
+        }
 
 }
