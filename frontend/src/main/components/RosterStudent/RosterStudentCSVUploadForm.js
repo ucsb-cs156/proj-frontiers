0e34f6bba14f293872e672967b4df6a193582445
Daniel Jensen <djensen2@outlook.com>
2025-07-22 22:13:13 -0700

    tweaked form so button doesn't take whole card

--- frontend/src/main/components/RosterStudent/RosterStudentCSVUploadForm.js
+++ frontend/src/main/components/RosterStudent/RosterStudentCSVUploadForm.js
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Button, Form, Row } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import React from "react";
 
 export default function RosterStudentCSVUploadForm({ submitAction }) {
@@ -11,31 +11,27 @@ export default function RosterStudentCSVUploadForm({ submitAction }) {
 
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
-      <Row>
-        <Form.Group className="mb-2">
-          <Form.Label htmlFor="upload">Upload Student Roster</Form.Label>
-          <Form.Control
-            data-testid="RosterStudentCSVUploadForm-upload"
-            id="upload"
-            type="file"
-            accept=".csv"
-            isInvalid={Boolean(errors.upload)}
-            {...register("upload", { required: true })}
-          />
-          <Form.Control.Feedback type="invalid">
-            {errors.upload && "Roster is required. "}
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Row>
-        <Button
-          type="submit"
-          data-testid="RosterStudentCSVUploadForm-submit"
-          className="mb-3"
-        >
-          Upload
-        </Button>
-      </Row>
+      <Form.Group className="mb-2">
+        <Form.Label htmlFor="upload">Upload Student Roster</Form.Label>
+        <Form.Control
+          data-testid="RosterStudentCSVUploadForm-upload"
+          id="upload"
+          type="file"
+          accept=".csv"
+          isInvalid={Boolean(errors.upload)}
+          {...register("upload", { required: true })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.upload && "Roster is required. "}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Button
+        type="submit"
+        data-testid="RosterStudentCSVUploadForm-submit"
+        className="mt-3"
+      >
+        Upload
+      </Button>
     </Form>
   );
 }
