31de48a60a9e372c63f7f69b5a48c0d140afe0ef
Daniel Jensen <djensen2@outlook.com>
2025-07-27 22:48:56 -0700

    dj - added backend pagination to the Users endpoint

--- src/main/java/edu/ucsb/cs156/frontiers/repositories/UserRepository.java
+++ src/main/java/edu/ucsb/cs156/frontiers/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package edu.ucsb.cs156.frontiers.repositories;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.ucsb.cs156.frontiers.entities.User;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 /**
@@ -19,6 +20,8 @@ public interface UserRepository extends CrudRepository<User, Long> {
    * @return Optional of User (empty if not found)
    */
   Optional<User> findByEmail(String email);
+
+  Page<User> findAll(Pageable pageable);
 
   Optional<User> findByGoogleSub(String googleSub);
 
--- src/test/java/edu/ucsb/cs156/frontiers/controllers/UsersControllerTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/controllers/UsersControllerTests.java
@@ -16,11 +16,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -80,9 +84,11 @@ public class UsersControllerTests extends ControllerTestCase {
     userDTOS.add(UserDataDTO.from(u2, false, true));
     userDTOS.add(UserDataDTO.from(u, false, false));
 
-    String expectedJson = mapper.writeValueAsString(userDTOS);
+    Page<UserDataDTO> page = new PageImpl<>(userDTOS);
 
-    when(mockUserDataDTOService.getUserDataDTOs()).thenReturn(userDTOS);
+    String expectedJson = mapper.writeValueAsString(page);
+
+    when(mockUserDataDTOService.getUserDataDTOs(any(Pageable.class))).thenReturn(page);
 
     
     // act
