3ba366542b75a09c555fe7df4ca7f896bafc1028
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-03 19:09:39 -0700

    pc - upgrade to storybook 9

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -53,7 +53,7 @@ export const decorators = [
 ];
 
 
-/** @type { import('@storybook/react').Preview } */
+/** @type { import('@storybook/react-webpack5').Preview } */
 const preview = {
   parameters: {
     controls: {
