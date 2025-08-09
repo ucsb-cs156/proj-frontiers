a64b10cdc306fad8962d448116b755b34c72b6e9
Daniel Jensen <djensen2@outlook.com>
2025-08-09 16:50:51 -0700

    dj - rewrote tests to use correct spring context, added default passthrough mockbean on other tests

--- src/main/java/edu/ucsb/cs156/frontiers/config/WebConfig.java
+++ src/main/java/edu/ucsb/cs156/frontiers/config/WebConfig.java
@@ -1,15 +1,14 @@
 package edu.ucsb.cs156.frontiers.config;
 
+import edu.ucsb.cs156.frontiers.interceptors.RoleUpdateInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import edu.ucsb.cs156.frontiers.interceptors.RoleUpdateInterceptor;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Autowired
     private RoleUpdateInterceptor roleUpdateInterceptor;
     
