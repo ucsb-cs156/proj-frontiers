0b11206337d17f6b5c73c316ef4783fea4feadb1
Phill Conrad <pconrad@cs.ucsb.edu>
2025-08-11 17:48:21 -0700

    pc - add stories for Help Pages

--- frontend/src/stories/pages/Help/HelpAboutPage.stories.js
+++ frontend/src/stories/pages/Help/HelpAboutPage.stories.js
@@ -1,13 +1,11 @@
-
 import HelpAboutPage from "main/pages/Help/HelpAboutPage";
 
 export default {
-    title: "pages/Help/HelpAboutPage",
-    component: HelpAboutPage,
+  title: "pages/Help/HelpAboutPage",
+  component: HelpAboutPage,
 };
 
 const Template = () => <HelpAboutPage />;
 
 export const Default = Template.bind({});
 Default.parameters = {};
-
