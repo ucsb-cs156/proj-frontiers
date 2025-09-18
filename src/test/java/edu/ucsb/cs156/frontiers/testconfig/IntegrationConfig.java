c5bf7f8e46510a7f932608a0be9ad52fa74e1875
Daniel Jensen <djensen2@outlook.com>
2025-09-18 11:19:13 -0700

    dj - added oidc wiremock

--- src/test/java/edu/ucsb/cs156/frontiers/testconfig/IntegrationConfig.java
+++ src/test/java/edu/ucsb/cs156/frontiers/testconfig/IntegrationConfig.java
@@ -1,35 +1,9 @@
 package edu.ucsb.cs156.frontiers.testconfig;
 
 import edu.ucsb.cs156.frontiers.config.SecurityConfig;
-import edu.ucsb.cs156.frontiers.services.CurrentUserService;
-import edu.ucsb.cs156.frontiers.services.GithubSignInService;
-import edu.ucsb.cs156.frontiers.services.GoogleSignInService;
-import edu.ucsb.cs156.frontiers.services.GrantedAuthoritiesService;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @TestConfiguration
 @Import(SecurityConfig.class)
-public class IntegrationConfig {
-
-  @Bean
-  public CurrentUserService currentUserService() {
-    return new MockCurrentUserServiceImpl();
-  }
-
-  @Bean
-  public GrantedAuthoritiesService grantedAuthoritiesService() {
-    return new GrantedAuthoritiesService();
-  }
-
-  @Bean
-  public GithubSignInService githubSignInService() {
-    return new MockGithubSignInService();
-  }
-
-  @Bean
-  public GoogleSignInService googleSignInService() {
-    return new MockGoogleSignInService();
-  }
-}
+public class IntegrationConfig {}
