aa02c0afc736ee95e40f7f059bea8726758ed730
Samprita Chakraborty <sampritac9@gmail.com>
2025-11-17 00:05:53 -0800

    added assignment db entity

---
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Assignment.java
@@ -0,0 +1,38 @@
+package edu.ucsb.cs156.frontiers.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "assignment")
+public class Assignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    @NotNull
+    @Pattern(regexp = "individual|team", message = "asn_type must be 'individual' or 'team'")
+    private String asn_type;
+
+    @NotNull
+    @Pattern(regexp = "public|private", message = "visibility must be 'public' or 'private'")
+    private String visibility;
+
+    @NotNull
+    @Pattern(regexp = "read|write|maintain|admin", message = "permission must be 'read', 'write', 'maintain', or 'admin'")
+    private String permission;
+}
