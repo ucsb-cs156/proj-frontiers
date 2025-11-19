f07a384779c5257a6e638c38f95c083bf0cc8ba1
Keigo Omura <komura@ucsb.edu>
2025-11-19 14:48:34 -0800

    changing repository initialization again

--- src/main/java/edu/ucsb/cs156/frontiers/repositories/AssignmentRepository.java
+++ src/main/java/edu/ucsb/cs156/frontiers/repositories/AssignmentRepository.java
@@ -3,5 +3,4 @@ package edu.ucsb.cs156.frontiers.repositories;
 import edu.ucsb.cs156.frontiers.entities.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {}
