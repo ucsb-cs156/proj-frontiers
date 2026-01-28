4ee204479aee255da76529f3fee1e01212b68a99
Summit Haque <summit.haque@gmail.com>
2026-01-27 17:27:56 -0800

    Added attribute jobname in the src/main/java/edu/ucsb/cs156/frontiers/entities/Job.java file

--- src/main/java/edu/ucsb/cs156/frontiers/entities/Job.java
+++ src/main/java/edu/ucsb/cs156/frontiers/entities/Job.java
@@ -29,6 +29,7 @@ public class Job {
   @LastModifiedDate private ZonedDateTime updatedAt;
 
   private String status;
+  private String jobName;
 
   // 1048576 is 2^20, which is the max size of a mediumtext in MySQL
   @Column(
