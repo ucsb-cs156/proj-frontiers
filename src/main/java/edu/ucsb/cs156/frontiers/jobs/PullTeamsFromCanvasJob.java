efecb6f8eed1d1ff288d2c8389553f55b13d4ad3
Daniel Jensen <djensen2@outlook.com>
2026-01-27 19:59:58 -0800

    dj - migrated canvas sync endpoints to their own controller, made teams sync into a job

---
+++ src/main/java/edu/ucsb/cs156/frontiers/jobs/PullTeamsFromCanvasJob.java
@@ -0,0 +1,90 @@
+package edu.ucsb.cs156.frontiers.jobs;
+
+import edu.ucsb.cs156.frontiers.entities.Course;
+import edu.ucsb.cs156.frontiers.entities.RosterStudent;
+import edu.ucsb.cs156.frontiers.entities.Team;
+import edu.ucsb.cs156.frontiers.entities.TeamMember;
+import edu.ucsb.cs156.frontiers.errors.DuplicateGroupException;
+import edu.ucsb.cs156.frontiers.models.CanvasGroup;
+import edu.ucsb.cs156.frontiers.repositories.TeamRepository;
+import edu.ucsb.cs156.frontiers.services.CanvasService;
+import edu.ucsb.cs156.frontiers.services.jobs.JobContext;
+import edu.ucsb.cs156.frontiers.services.jobs.JobContextConsumer;
+import edu.ucsb.cs156.frontiers.utilities.CanonicalFormConverter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lombok.Builder;
+import org.springframework.validation.annotation.Validated;
+
+@Builder
+@Validated
+public class PullTeamsFromCanvasJob implements JobContextConsumer {
+
+  Course course;
+  String groupsetId;
+  CanvasService canvasService;
+  TeamRepository teamRepository;
+
+  @Override
+  public void accept(JobContext ctx) throws Exception {
+    ctx.log("Processing...");
+    List<CanvasGroup> groups = canvasService.getCanvasGroups(course, groupsetId);
+    HashMap<String, RosterStudent> mappedStudents = new HashMap<>();
+    HashMap<String, Team> teamsByName = new HashMap<>();
+    HashMap<Integer, Team> teamsByCanvasId = new HashMap<>();
+    course.getRosterStudents().forEach(student -> mappedStudents.put(student.getEmail(), student));
+    course
+        .getTeams()
+        .forEach(
+            team -> {
+              teamsByName.put(team.getName(), team);
+              if (team.getCanvasId() != null) {
+                teamsByCanvasId.put(team.getCanvasId(), team);
+              }
+            });
+
+    List<Team> createdTeams = new ArrayList<>();
+
+    for (CanvasGroup group : groups) {
+      Team linked;
+      if (teamsByCanvasId.containsKey(group.getId())) {
+        linked = teamsByCanvasId.get(group.getId());
+      } else if (teamsByName.containsKey(group.getName().trim())) {
+        linked = teamsByName.get(group.getName().trim());
+        if (linked.getCanvasId() != null && !linked.getCanvasId().equals(group.getId())) {
+          throw new DuplicateGroupException();
+        }
+      } else {
+        linked =
+            Team.builder()
+                .name(group.getName())
+                .teamMembers(new ArrayList<>())
+                .course(course)
+                .build();
+      }
+
+      linked.setCanvasId(group.getId());
+      Team finalLinked = linked;
+      group
+          .getMembers()
+          .forEach(
+              email -> {
+                RosterStudent student =
+                    mappedStudents.get(CanonicalFormConverter.convertToValidEmail(email));
+                if (student != null) {
+                  if (student.getTeamMembers().stream()
+                      .anyMatch(teamMember -> teamMember.getTeam().equals(finalLinked))) {
+                    return;
+                  } else {
+                    finalLinked
+                        .getTeamMembers()
+                        .add(TeamMember.builder().team(finalLinked).rosterStudent(student).build());
+                  }
+                }
+              });
+      createdTeams.add(finalLinked);
+    }
+    teamRepository.saveAll(createdTeams);
+  }
+}
