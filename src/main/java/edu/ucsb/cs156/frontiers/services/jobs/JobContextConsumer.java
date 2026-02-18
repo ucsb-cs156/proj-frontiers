03825f13f3e78e88e6cf6343a1f874fd3f201e73
Summit Haque <summit.haque@gmail.com>
2026-02-18 11:54:39 -0800

    A getCourse() method is added in the JobContextConsumer interface. It will allow the JobService method to get course object from all jobs associated with any course

--- src/main/java/edu/ucsb/cs156/frontiers/services/jobs/JobContextConsumer.java
+++ src/main/java/edu/ucsb/cs156/frontiers/services/jobs/JobContextConsumer.java
@@ -1,6 +1,12 @@
 package edu.ucsb.cs156.frontiers.services.jobs;
 
+import edu.ucsb.cs156.frontiers.entities.Course;
+
 @FunctionalInterface
 public interface JobContextConsumer {
   void accept(JobContext c) throws Exception;
+
+  default Course getCourse() {
+    return null;
+  }
 }
