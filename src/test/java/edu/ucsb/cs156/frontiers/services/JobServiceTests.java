51abc94abf614b9d96b5b1e359a703b8c0293c13
Summit Haque <summit.haque@gmail.com>
2026-02-18 13:52:59 -0800

    Test added in src/test/java/edu/ucsb/cs156/frontiers/services/JobServiceTests.java for the course part in JobService

--- src/test/java/edu/ucsb/cs156/frontiers/services/JobServiceTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/services/JobServiceTests.java
@@ -10,6 +10,7 @@ import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+import edu.ucsb.cs156.frontiers.entities.Course;
 import edu.ucsb.cs156.frontiers.entities.Job;
 import edu.ucsb.cs156.frontiers.entities.User;
 import edu.ucsb.cs156.frontiers.jobs.TestJob;
@@ -92,13 +93,40 @@ public class JobServiceTests {
 
   @Test
   void runAsJob_fires_correctly() {
-    TestJob job = TestJob.builder().fail(false).sleepMs(0).build();
+    Course course = Course.builder().id(1L).courseName("Test Course").build();
+
+    TestJob job = TestJob.builder().fail(false).sleepMs(0).course(course).build();
 
     Job fireJob =
         Job.builder()
             .jobName("TestJob")
             .createdBy(user.getUser())
             .userEmail(user.getUser().getEmail())
+            .courseName("Test Course")
+            .course(course)
+            .status("running")
+            .build();
+
+    doNothing().when(injectedJobService).runJobAsync(any(), any());
+    when(currentUserService.getUser()).thenReturn(user.getUser());
+
+    MatcherAssert.assertThat(fireJob, samePropertyValuesAs(jobService.runAsJob(job)));
+    verify(jobRepository).save(eq(fireJob));
+    verify(injectedJobService).runJobAsync(eq(fireJob), eq(job));
+  }
+
+  @Test
+  void runAsJob_withNullCourse_setsCourseNameToNull() {
+
+    TestJob job = TestJob.builder().fail(false).sleepMs(0).course(null).build();
+
+    Job fireJob =
+        Job.builder()
+            .jobName("TestJob")
+            .createdBy(user.getUser())
+            .userEmail(user.getUser().getEmail())
+            .courseName(null)
+            .course(null)
             .status("running")
             .build();
 
