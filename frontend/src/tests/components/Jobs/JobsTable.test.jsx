b0ac0dc7ed5dec9374b3de2fd50024a5c1e69b28
Summit Haque <summit.haque@gmail.com>
2026-02-18 15:28:21 -0800

    Course Name column added in the test file of Job Status table

--- frontend/src/tests/components/Jobs/JobsTable.test.jsx
+++ frontend/src/tests/components/Jobs/JobsTable.test.jsx
@@ -37,6 +37,7 @@ describe("JobsTable tests", () => {
         id: 1,
         jobName: "Test Job",
         userEmail: "test@example.com",
+        courseName: "CS 156",
         createdAt: "2023-01-01T10:00:00",
         updatedAt: "2023-01-01T10:05:00",
         status: "complete",
@@ -56,6 +57,7 @@ describe("JobsTable tests", () => {
     expect(screen.getByText("id")).toBeInTheDocument();
     expect(screen.getByText("Job Name")).toBeInTheDocument();
     expect(screen.getByText("User Email")).toBeInTheDocument();
+    expect(screen.getByText("Course Name")).toBeInTheDocument();
     expect(screen.getByText("Created")).toBeInTheDocument();
     expect(screen.getByText("Updated")).toBeInTheDocument();
     expect(screen.getByText("Status")).toBeInTheDocument();
@@ -65,6 +67,7 @@ describe("JobsTable tests", () => {
     expect(screen.getByText("1")).toBeInTheDocument();
     expect(screen.getByText("Test Job")).toBeInTheDocument();
     expect(screen.getByText("test@example.com")).toBeInTheDocument();
+    expect(screen.getByText("CS 156")).toBeInTheDocument();
     expect(screen.getByText("2023-01-01 10:00:00")).toBeInTheDocument();
     expect(screen.getByText("2023-01-01 10:05:00")).toBeInTheDocument();
     expect(screen.getByText("complete")).toBeInTheDocument();
