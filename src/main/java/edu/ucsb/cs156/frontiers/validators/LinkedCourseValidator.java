a634f9a7a0b3ae9ab75aa39b5cf5b48259353b1c
Phill Conrad <pconrad@cs.ucsb.edu>
2026-02-19 16:12:04 -0800

    pc/dj - add javadoc comment to validator

--- src/main/java/edu/ucsb/cs156/frontiers/validators/LinkedCourseValidator.java
+++ src/main/java/edu/ucsb/cs156/frontiers/validators/LinkedCourseValidator.java
@@ -4,6 +4,14 @@ import edu.ucsb.cs156.frontiers.entities.Course;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * This is a validator, which enforces a property. These are used together with @Validated to in a
+ * spring managed bean (for example, a controller or service).
+ *
+ * <p>With this validator, you would have to put lots of code in your controller to check for linked
+ * courses. Instead, we can just put @HasLinkedCanvasCourse on the method parameters that require
+ * it, and if the contraint is not satisfied, a ConstraintViolationException will be thrown.
+ */
 public class LinkedCourseValidator implements ConstraintValidator<HasLinkedCanvasCourse, Course> {
 
   @Override
