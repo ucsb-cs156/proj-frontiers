20e24b023b3fe123104c98bb815862b838971411
Summit Haque <summit.haque@gmail.com>
2026-03-03 12:07:20 -0800

    Added method that find jobs by course in src/main/java/edu/ucsb/cs156/frontiers/repositories/JobsRepository.java

--- src/main/java/edu/ucsb/cs156/frontiers/repositories/JobsRepository.java
+++ src/main/java/edu/ucsb/cs156/frontiers/repositories/JobsRepository.java
@@ -1,8 +1,11 @@
 package edu.ucsb.cs156.frontiers.repositories;
 
 import edu.ucsb.cs156.frontiers.entities.Job;
+import org.springdoc.core.converters.models.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobsRepository extends JpaRepository<Job, Long> {}
+public interface JobsRepository extends JpaRepository<Job, Long> {
+  Iterable<Job> findByCourseId(Long courseId, Sort sort);
+}
