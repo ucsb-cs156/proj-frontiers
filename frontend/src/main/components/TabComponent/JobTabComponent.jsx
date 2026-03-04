19418da221e3c2c7479e6ff835b6a840fd996e6f
Summit Haque <summit.haque@gmail.com>
2026-03-04 15:43:05 -0800

    Added a refresh button in job tab in frontend/src/main/components/TabComponent/JobTabComponent.jsx

--- frontend/src/main/components/TabComponent/JobTabComponent.jsx
+++ frontend/src/main/components/TabComponent/JobTabComponent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useBackend } from "main/utils/useBackend";
 import JobsTable from "main/components/Jobs/JobsTable";
+import { Button } from "react-bootstrap";
 
 export default function JobTabComponent({ courseId, testIdPrefix }) {
-  const { data: jobs } = useBackend(
-    [`/api/jobs/course`, courseId],
+  const { data: jobs, refetch } = useBackend(
+    ["/api/jobs/course", courseId],
     {
       method: "GET",
       url: "/api/jobs/course",
@@ -13,9 +14,22 @@ export default function JobTabComponent({ courseId, testIdPrefix }) {
     [],
   );
 
+  const refreshJobs = () => {
+    refetch();
+  };
+
   return (
     <div data-testid={`${testIdPrefix}-jobs-tab`}>
       <h4 className="mb-3">Job Status</h4>
+
+      <Button
+        className="mb-3"
+        onClick={refreshJobs}
+        data-testid={`${testIdPrefix}-refresh-jobs`}
+      >
+        Refresh
+      </Button>
+
       <JobsTable jobs={jobs} />
     </div>
   );
