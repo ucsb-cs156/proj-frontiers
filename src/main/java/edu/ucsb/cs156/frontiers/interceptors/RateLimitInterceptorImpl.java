7ca0409d21a8970a940629a43d6b0b62c8d58223
Daniel Jensen <djensen2@outlook.com>
2026-03-04 16:51:52 -0800

    dj - added way to track rate limiting by github

---
+++ src/main/java/edu/ucsb/cs156/frontiers/interceptors/RateLimitInterceptorImpl.java
@@ -0,0 +1,60 @@
+package edu.ucsb.cs156.frontiers.interceptors;
+
+import edu.ucsb.cs156.frontiers.entities.RateLimitDataPoint;
+import edu.ucsb.cs156.frontiers.repositories.RateLimitDataPointRepository;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RateLimitInterceptorImpl implements RateLimitInterceptor {
+
+  private final RateLimitDataPointRepository rateLimitDataPointRepository;
+
+  public RateLimitInterceptorImpl(RateLimitDataPointRepository rateLimitDataPointRepository) {
+    this.rateLimitDataPointRepository = rateLimitDataPointRepository;
+  }
+
+  @Override
+  public ClientHttpResponse intercept(
+      HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+
+    if (!request.getURI().getHost().equals("api.github.com")) {
+      return execution.execute(request, body);
+    }
+
+    if (!request.getURI().getPath().contains("/app/installations/")) {
+      return execution.execute(request, body);
+    }
+
+    String installationId =
+        request.getURI().getPath().replace("/app/installations/", "").replace("/access_tokens", "");
+
+    ClientHttpResponse response = execution.execute(request, body);
+    RateLimitDataPoint newPoint =
+        RateLimitDataPoint.builder().installationId(installationId).build();
+
+    List<String> headers = response.getHeaders().get("X-RateLimit-Remaining");
+
+    if (headers == null || headers.isEmpty()) {
+      return response;
+    }
+    try {
+      long remaining = Long.parseLong(headers.getFirst());
+      newPoint.setRemaining(remaining);
+      saveDataPoint(newPoint);
+    } catch (NumberFormatException e) {
+      return response;
+    }
+    return response;
+  }
+
+  @Async
+  public void saveDataPoint(RateLimitDataPoint point) {
+    rateLimitDataPointRepository.save(point);
+  }
+}
