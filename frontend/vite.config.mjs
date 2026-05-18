aceb20a84dee3e78fefa71cbe3e0c69561435f72
Parth Mohnot <pmohnot@ucsb.edu>
2026-05-18 11:29:44 -0700

    PM - configure default backend port to 8081 due to 8080 conflict

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -53,7 +53,7 @@ export default defineConfig({
     server: {
         port: 3000,
         proxy: {
-            "/api": "http://localhost:8080",
+            "/api": "http://localhost:8081",
         },
     },
 });
