87db2dc643eebf2fffee02044aa1635e61c3ea0c
Brian Hu <bjhu834@ucsb.edu>
2026-05-21 17:18:57 -0700

    Added more tests

--- src/test/java/edu/ucsb/cs156/frontiers/jobs/DeleteRepoJobTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/jobs/DeleteRepoJobTests.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.frontiers.jobs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
