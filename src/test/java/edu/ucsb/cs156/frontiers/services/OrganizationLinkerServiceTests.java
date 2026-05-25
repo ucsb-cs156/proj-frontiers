1862ccc8836ccd66382c69f687b7f0f6a4af30b7
Nini Yen <h_yen@ucsb.edu>
2026-05-24 20:18:13 -0700

    Cover null default base permission warning

--- src/test/java/edu/ucsb/cs156/frontiers/services/OrganizationLinkerServiceTests.java
+++ src/test/java/edu/ucsb/cs156/frontiers/services/OrganizationLinkerServiceTests.java
@@ -298,6 +298,32 @@ public class OrganizationLinkerServiceTests {
   }
 
   @Test
+  public void test_no_default_base_permission_warning_when_permission_is_null() throws Exception {
+    Course course = Course.builder().orgName("ucsb-cs156").installationId("12345").build();
+    when(provider.getNow())
+        .thenReturn(Optional.of(ZonedDateTime.of(2025, 3, 11, 0, 0, 0, 0, ZoneId.of("UTC"))));
+    doReturn("definitely.real.jwt").when(jwtService).getInstallationToken(eq(course));
+    doReturn(null).when(githubGraphQLService).getDefaultBasePermission(eq(course));
+    String apiResponse =
+        """
+            {
+              "created_at": "2024-10-11T04:33:35Z"
+            }
+            """;
+    mockRestServiceServer
+        .expect(requestTo("https://api.github.com/orgs/ucsb-cs156"))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", "Bearer definitely.real.jwt"))
+        .andExpect(header("Accept", "application/vnd.github+json"))
+        .andExpect(header("X-GitHub-Api-Version", "2022-11-28"))
+        .andRespond(withSuccess(apiResponse, MediaType.APPLICATION_JSON));
+
+    CourseWarning warning = organizationLinkerService.checkCourseWarnings(course);
+    assertFalse(warning.showOrganizationAgeWarning());
+    assertFalse(warning.showDefaultBasePermissions());
+  }
+
+  @Test
   public void no_rest_service_calls_when_not_installed() throws Exception {
     Course course = Course.builder().orgName("ucsb-cs156").build();
     CourseWarning warning = organizationLinkerService.checkCourseWarnings(course);
